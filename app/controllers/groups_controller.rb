f35c0e5db2edae86dea30fae796b272f6738fbf5
宋沛雨 <spy@songpeiyudeMacBook-Pro.local>
2017-04-03 16:34:33 +0800

    post: edit&delete

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,7 @@ end
 
 def show
   @group = Group.find(params[:id])
-  @posts = @group.post.recent.paginate(:page => params[:page], :per_page => 5)
+  @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
 
 end
 
